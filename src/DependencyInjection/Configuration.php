291088bd899f89171fa8e944591b2965628430a6
Mario Lubenka <mario.lubenka@googlemail.com>
2025-10-27 23:24:26 +0100

    fix: signature

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -23,7 +23,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('form_builder');
         $rootNode = $treeBuilder->getRootNode();
